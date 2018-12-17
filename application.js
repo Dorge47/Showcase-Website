1b1b2017d43af1cef37231b76d1265a2331e9914
Dorge47 <Chris_jandrade@yahoo.com>
2018-12-17 15:25:25 -0800

    Commit in-class changes 12-17

--- application.js
+++ application.js
@@ -1,4 +1,4 @@
-isNavBarDropped = 0
+isNavBarDropped = 1
 
 function showNavBar() {
     if (isNavBarDropped) {
@@ -7,5 +7,17 @@ function showNavBar() {
     } else {
         document.getElementById("navdrop").classList.add("nav-dropped");
         isNavBarDropped = 1;
+    }
+}
+
+function brightenImage(imageNumber) {
+    if (window.innerWidth >= 1024/*Disable function on mobile and tablets*/) {
+        document.getElementsByClassName(`img${imageNumber}`)[0].parentNode.style.filter = "brightness(100%)"
+    }
+}
+
+function dimImage(imageNumber) {
+    if (window.innerWidth >= 1024/*Disable function on mobile and tablets*/) {
+        document.getElementsByClassName(`img${imageNumber}`)[0].parentNode.style.filter = ""
     }
 }
