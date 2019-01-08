fd621c36481879426c67257170cfa5f2d6d521db
Dorge47 <Chris_jandrade@yahoo.com>
2019-01-08 15:27:53 -0800

    Commit in-class changes 1-8

--- application.js
+++ application.js
@@ -21,3 +21,24 @@ function dimImage(imageNumber) {
         document.getElementsByClassName(`img${imageNumber}`)[0].parentNode.style.filter = ""
     }
 }
+
+function toggleOverlay(overlayNumber) {
+    if (document.getElementById(`overlay${overlayNumber}`).overlaystatus == 1) {
+        hideOverlay(overlayNumber)
+    }
+    else {
+        showOverlay(overlayNumber)
+    }
+}
+
+function showOverlay(overlayNumber) {
+    document.getElementById(`overlay${overlayNumber}`).style.display = "block";
+    document.getElementById(`overlay${overlayNumber}`).previousSibling.previousSibling.style.filter = "brightness(50%)";
+    document.getElementById(`overlay${overlayNumber}`).overlaystatus = 1;
+}
+
+function hideOverlay(overlayNumber) {
+    document.getElementById(`overlay${overlayNumber}`).style.display = "none";
+    document.getElementById(`overlay${overlayNumber}`).previousSibling.previousSibling.style.filter = "brightness(100%)";
+    document.getElementById(`overlay${overlayNumber}`).overlaystatus = 0;
+}
